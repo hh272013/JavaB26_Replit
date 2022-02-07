05b5d8718741b1aa6be296bbad61e2dd7d5be44a
AliParmar <ali.parmar@att.net>
2022-02-06 22:33:04 -0600

    2/6/2022

---
+++ src/Assignment_02_06/Print_Variable_2.java
@@ -0,0 +1,15 @@
+package Assignment_02_06;
+/*
+Given a String variable message, write a statement that displays the value of the variable
+Remember: Variables hold values and those values are used whenever the variable is used
+Main topics: print statement, variables, Strings
+ */
+public class Print_Variable_2 {
+    public static void main(String[] args) {
+        //DO NOT TOUCH THESE LINE/LINES.
+        String message = "I am a Cydeo school student.";
+
+        //WRITE YOUR CODE BELOW THIS LINE:
+        System.out.println(message);
+    }
+}
