afd20711756a4b9c6ba8a1ddb8aabc78cd225924
AliParmar <ali.parmar@att.net>
2022-02-23 12:10:24 -0600

    Updated

--- mehmet_only/day01_0215/Practice.java
+++ mehmet_only/day01_0215/Practice.java
@@ -3,5 +3,6 @@ package day01_0215;
 public class Practice {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("This is an update");
     }
 }
