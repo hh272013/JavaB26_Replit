dd4a7d4297128e2b0f8a02822d2af9a2846c6bfb
AliParmar <ali.parmar@att.net>
2022-02-23 11:59:47 -0600

    update

--- replit/Assignment_02_16/test.java
+++
@@ -1,9 +0,0 @@
-package Assignment_02_16;
-
-public class test {
-    public static void main(String[] args) {
-        int a = 1;
-        a = -a-- + a++ / -a-- * --a;
-        System.out.println(a);
-    }
-}
