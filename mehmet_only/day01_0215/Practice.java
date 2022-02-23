41fd56459cf5e203d37ccec35b1b71fdd75352c2
Ali Parmar <66921407+AliParmar@users.noreply.github.com>
2022-02-23 12:36:56 -0600

    Delete mehmet_only/day01_0215 directory

--- mehmet_only/day01_0215/Practice.java
+++
@@ -1,9 +0,0 @@
-package day01_0215;
-
-public class Practice {
-    public static void main(String[] args) {
-
-        System.out.println("This is an update");
-        System.out.println("This is an update from gitHub");
-    }
-}
