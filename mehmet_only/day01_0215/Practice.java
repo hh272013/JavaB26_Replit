e62f100819b8a1cf1123033c193b93961fd3b34b
AliParmar <ali.parmar@att.net>
2022-02-23 12:22:17 -0600

    Desktop_GitHub
    Commit to master

--- mehmet_only/day01_0215/Practice.java
+++ mehmet_only/day01_0215/Practice.java
@@ -4,5 +4,6 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("This is an update");
+        System.out.println("This is another update");
     }
 }
