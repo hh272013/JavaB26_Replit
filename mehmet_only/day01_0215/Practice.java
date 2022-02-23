069e17d5f0278654dadeb831dd2adc70a0f79329
Ali Parmar <66921407+AliParmar@users.noreply.github.com>
2022-02-23 12:25:29 -0600

    Updated
    Updated from GitHub

--- mehmet_only/day01_0215/Practice.java
+++ mehmet_only/day01_0215/Practice.java
@@ -4,6 +4,6 @@ public class Practice {
     public static void main(String[] args) {
 
         System.out.println("This is an update");
-
+        System.out.println("This is an update from gitHub");
     }
 }
